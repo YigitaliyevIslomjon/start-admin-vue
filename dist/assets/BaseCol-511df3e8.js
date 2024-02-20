86036584a388c3a246dea2d980fb8b800336e269
Azamjon Kirgizboev <azamjon_k@asakabank.uz>
2024-02-20 15:44:20 +0500

    modified

--- dist/assets/BaseCol-511df3e8.js
+++
@@ -1,1 +0,0 @@
-import{o as a,k as l,j as r,N as t}from"./index-fd7b041c.js";const p={__name:"BaseCol",props:{colClass:{type:String}},setup(s){const e=s;return(o,c)=>(a(),l("div",{class:t(["p-2",e.colClass])},[r(o.$slots,"default")],2))}};export{p as _};
--- dist/assets/index-f8fe1c2c.js
+++
@@ -1,1 +0,0 @@
-import{a0 as o,r,o as n,g as t}from"./index-fd7b041c.js";const c={};function s(_,a){const e=r("router-view");return n(),t(e)}const i=o(c,[["render",s]]);export{i as default};
