c9677d1839d16d5fc5b31724626162aa672afdee
hayitmurod707 <hayitmurod707@gmail.com>
2024-02-29 10:43:51 +0500

    deleted dist folder

--- dist/assets/ArrowDownIcon-67892e31.js
+++
@@ -1,1 +0,0 @@
-import{o,k as e,i as t}from"./index-de5e105f.js";const n={xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 10 12"},r=t("path",{stroke:"#767994","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"1.3",d:"M5 .667v10.667m0 0 4-4m-4 4-4-4"},null,-1),s=[r];function c(i,a){return o(),e("svg",n,s)}const l={render:c};export{l as default,c as render};
