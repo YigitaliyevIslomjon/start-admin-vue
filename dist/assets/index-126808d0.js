dff92933f728719e9ac7f86a58bca5dec3e0f431
imuhammadibragimov1994@gmail.com <muhammad1994muhammad>
2024-07-27 10:20:46 +0500

    delete dist

--- dist/assets/index-126808d0.js
+++
@@ -1,1 +0,0 @@
-import{a0 as e,o as n,k as t}from"./index-2b674f94.js";const c={};function r(a,o){return n(),t("div",null," General Settings Page ")}const _=e(c,[["render",r]]);export{_ as default};
--- dist/assets/overlayeventbus.esm-0786733b.js
+++
@@ -1,1 +0,0 @@
-import{aG as r}from"./index-2b674f94.js";var e=r();export{e as O};
