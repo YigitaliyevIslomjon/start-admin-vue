52de25b66293d85da7b74c8d6abf05ed1f5fc2a3
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-08-18 22:37:22 +0500

    push

--- dist/assets/index.service-ebf66f0e.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./axios.config-b7593ba8.js";const o={compose:"compose/1"},n=t=>e.get(`${o.compose}/`,t),m=t=>e.post(`${o.compose}/`,t),p=({id:t,body:c})=>e.put(`${o.compose}/${t}/`,c),r=t=>e.get(`${o.compose}/${t}/`),u=({id:t,body:c})=>e.put(`${o.compose}/${t}/custom-update/`,c);export{u as a,r as b,p as c,m as d,n as f};
--- dist/assets/overlayeventbus.esm-c356f942.js
+++
@@ -1,1 +0,0 @@
-import{bZ as r}from"./index-23b824e6.js";var e=r();export{e as O};
