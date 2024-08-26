d907fd5f931e44c8e2421b5087201dff52acdecf
Azam_John <counterkingbarcelona@gmail.com>
2024-08-26 23:07:38 +0500

    modified

--- dist/assets/index-7400dc36.js
+++
@@ -1,1 +0,0 @@
-import{p as n}from"./index.store-35780b7e.js";import{_ as t}from"./TheNavigation-613350f9.js";import{r as s,o as a,k as c,p as e,q as p,F as _}from"./index-d830192c.js";const d={__name:"index",setup(m){const o=n();return(l,i)=>{const r=s("router-view");return a(),c(_,null,[e(t,{navs:p(o).routes.children},null,8,["navs"]),e(r)],64)}}};export{d as default};
--- dist/assets/overlayeventbus.esm-1093a3d2.js
+++
@@ -1,1 +0,0 @@
-import{c1 as r}from"./index-d830192c.js";var e=r();export{e as O};
