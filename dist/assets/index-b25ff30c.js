78afe3287ec667bbb7cd3c6f391e922ea55ccf25
Azamjon Kirgizboev <azamjon_k@asakabank.uz>
2024-08-26 08:30:11 +0500

    builddelete

---
+++ dist/assets/index-b25ff30c.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index.store-10e50cba.js";import{_ as t}from"./TheNavigation-022d0317.js";import{r as s,o as a,k as c,p as e,q as p,F as _}from"./index-e979edcd.js";const d={__name:"index",setup(m){const o=n();return(l,i)=>{const r=s("router-view");return a(),c(_,null,[e(t,{navs:p(o).routes.children},null,8,["navs"]),e(r)],64)}}};export{d as default};
---
+++ dist/assets/overlayeventbus.esm-b243deb2.js
@@ -0,0 +1,1 @@
+import{c1 as r}from"./index-e979edcd.js";var e=r();export{e as O};
