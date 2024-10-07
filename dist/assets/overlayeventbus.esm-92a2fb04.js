2b429c83b207ffc39ef03abb221887544128884d
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-10-07 20:53:07 +0500

    push

---
+++ dist/assets/overlayeventbus.esm-92a2fb04.js
@@ -0,0 +1,1 @@
+import{c4 as r}from"./index-890c7e1c.js";var e=r();export{e as O};
---
+++ dist/assets/user-permissions.store-42ef17d8.js
@@ -0,0 +1,1 @@
+import{b0 as e}from"./index-890c7e1c.js";import{c as i}from"./axios.config-302f08b3.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),m=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{m as u};
