91dc19569c5094719e13b6399c81baa34857310e
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-10-12 19:16:09 +0500

    delete dist

--- dist/assets/user-permissions.store-66ea52a3.js
+++
@@ -1,1 +0,0 @@
-import{b2 as e}from"./index-4b760b17.js";import{c as i}from"./axios.config-c625a785.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),m=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{m as u};
