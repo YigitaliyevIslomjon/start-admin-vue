1aceb00614f50b97bab1540617da0ff68d4a7b66
Azam_John <counterkingbarcelona@gmail.com>
2024-10-22 11:54:50 +0500

    build

---
+++ dist/assets/overlayeventbus.esm-1bc81b2a.js
@@ -0,0 +1,1 @@
+import{c5 as r}from"./index-e614c07f.js";var e=r();export{e as O};
---
+++ dist/assets/user-permissions.store-4188289b.js
@@ -0,0 +1,1 @@
+import{b2 as e}from"./index-e614c07f.js";import{c as i}from"./axios.config-efc6db57.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),m=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{m as u};
