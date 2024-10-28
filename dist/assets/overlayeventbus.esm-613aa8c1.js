884fa147212ec24bbf0f63f8e5727194e2602f02
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-10-28 23:46:15 +0500

    delete dist

--- dist/assets/overlayeventbus.esm-613aa8c1.js
+++
@@ -1,1 +0,0 @@
-import{c7 as r}from"./index-9787d42e.js";var e=r();export{e as O};
--- dist/assets/pagination.store-f9a5153d.js
+++
@@ -1,1 +0,0 @@
-import{b3 as t}from"./index-9787d42e.js";const i=t("pagination-stores",{state:()=>({page:1,pageSize:10,firstRow:0}),actions:{resetPagination(){this.page=1,this.pageSize=10,this.firstRow=0}}});export{i as u};
