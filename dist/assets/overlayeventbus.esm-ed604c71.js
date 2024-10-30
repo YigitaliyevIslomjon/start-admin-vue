de95160e370109587f9d8f627abbaee2cc74e3bb
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-10-30 14:49:06 +0500

    deploy dist

---
+++ dist/assets/overlayeventbus.esm-ed604c71.js
@@ -0,0 +1,1 @@
+import{c7 as r}from"./index-4ef6da2a.js";var e=r();export{e as O};
---
+++ dist/assets/user-permissions.store-3b1991fa.js
@@ -0,0 +1,1 @@
+import{b3 as e}from"./index-4ef6da2a.js";import{c as i}from"./axios.config-c81aa6fe.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),m=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{m as u};
