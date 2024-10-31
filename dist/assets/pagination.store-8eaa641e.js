da42863d07ade46f5b3c4196f0cfdc3d282cb56b
Muhammad <imuhammadibragimov1994@gmail.com>
2024-10-31 13:22:36 +0500

    deploy dist

---
+++ dist/assets/pagination.store-8eaa641e.js
@@ -0,0 +1,1 @@
+import{b1 as t}from"./index-dd537147.js";const i=t("pagination-stores",{state:()=>({page:1,pageSize:10,firstRow:0}),actions:{resetPagination(){this.page=1,this.pageSize=10,this.firstRow=0}}});export{i as u};
