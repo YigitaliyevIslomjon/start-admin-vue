f9469ca3f9370c6dbbcaa51425048cc72ce8dcbc
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-11-07 23:40:17 +0500

    deploy dist

---
+++ dist/assets/overlayeventbus.esm-cb72fb4e.js
@@ -0,0 +1,1 @@
+import{c5 as r}from"./index-76bca116.js";var e=r();export{e as O};
---
+++ dist/assets/pagination.store-4fa4bb80.js
@@ -0,0 +1,1 @@
+import{ak as t}from"./index-76bca116.js";const i=t("pagination-stores",{state:()=>({page:1,pageSize:10,firstRow:0}),actions:{resetPagination(){this.page=1,this.pageSize=10,this.firstRow=0}}});export{i as u};
