a0114326bdfa294f4b9ccfa3ffcab6b67ed1639f
Yigitaliyev islomjon <islomjon.yigitaliyev@upay.uz>
2024-12-03 15:01:25 +0500

    clear dis

--- dist/assets/ArrowRightDownIcon-1fe78017.js
+++
@@ -1,1 +0,0 @@
-import{o,c as e,a as t}from"./index-0e7eaa7b.js";const n={xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24"},r=t("path",{stroke:"#1C274C","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",d:"m6 6 12 12m0 0V9m0 9H9"},null,-1),s=[r];function c(a,i){return o(),e("svg",n,s)}const l={render:c};export{l as default,c as render};
