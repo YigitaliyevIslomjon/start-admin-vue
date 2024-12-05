c9916c928c6b05a449041ae1fd6b741b13309720
Azam_John <counterkingbarcelona@gmail.com>
2024-12-05 23:20:52 +0500

    build

---
+++ dist/assets/Home-cf618ff7.js
@@ -0,0 +1,1 @@
+import{al as e,o,c}from"./index-31c8898b.js";const n={},a={class:"kanban-home flex w-full"};function s(t,r){return o(),c("div",a," Kanban home ")}const l=e(n,[["render",s]]);export{l as default};
---
+++ dist/assets/index-1c7f0f37.js
@@ -0,0 +1,1 @@
+import{al as e,o as n,c}from"./index-31c8898b.js";const t={};function r(a,o){return n(),c("div",null," General Settings Page ")}const l=e(t,[["render",r]]);export{l as default};
