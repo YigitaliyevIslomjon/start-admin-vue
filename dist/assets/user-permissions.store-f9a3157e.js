dce7d27289c150cc230e56aef76154b4d4b1d9d8
Azam_John <counterkingbarcelona@gmail.com>
2024-12-07 15:09:45 +0500

    fix: business trip decree local fixed and gets notices data from notice

--- dist/assets/user-permissions.store-f9a3157e.js
+++
@@ -1,1 +0,0 @@
-import{aC as e}from"./index-31c8898b.js";import{c as i}from"./axios.config-59c74e4e.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),a=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{a as u};
