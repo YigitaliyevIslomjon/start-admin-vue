396188b267bea7a455390ff0f5e523b713f8d810
Yigitaliyev islomjon <islomjon.yigitaliyev@upay.uz>
2024-12-12 23:33:05 +0500

    for deploy

--- dist/assets/index-fe58aaa1.js
+++
@@ -1,1 +0,0 @@
-import{al as e,o as n,c}from"./index-68cc6083.js";const t={};function r(a,o){return n(),c("div",null," General Settings Page ")}const l=e(t,[["render",r]]);export{l as default};
--- dist/assets/overlayeventbus.esm-bc700c6b.js
+++
@@ -1,1 +0,0 @@
-import{cc as r}from"./index-68cc6083.js";var e=r();export{e as O};
