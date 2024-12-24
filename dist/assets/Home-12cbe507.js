8e2a54bd25438e9388559092c70a90cf2fd2c2d6
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-12-24 17:42:14 +0500

    delete dist

--- dist/assets/Home-12cbe507.js
+++
@@ -1,1 +0,0 @@
-import{al as e,o,c}from"./index-a8bb9687.js";const n={},a={class:"kanban-home flex w-full"};function s(t,r){return o(),c("div",a," Kanban home ")}const l=e(n,[["render",s]]);export{l as default};
--- dist/assets/overlayeventbus.esm-3bcec105.js
+++
@@ -1,1 +0,0 @@
-import{c1 as r}from"./index-a8bb9687.js";var e=r();export{e as O};
