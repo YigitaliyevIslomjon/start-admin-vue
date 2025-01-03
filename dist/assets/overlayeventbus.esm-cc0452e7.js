9b3df8096baee8de2ab265d5b800f2487a4c8b7b
Yigitaliyev islomjon <islomjon.yigitaliyev@upay.uz>
2025-01-03 18:10:41 +0500

    build

---
+++ dist/assets/overlayeventbus.esm-cc0452e7.js
@@ -0,0 +1,1 @@
+import{c1 as r}from"./index-07f23a1c.js";var e=r();export{e as O};
---
+++ dist/assets/user-permissions.store-41f28035.js
@@ -0,0 +1,1 @@
+import{aD as e}from"./index-07f23a1c.js";import{c as i}from"./axios.config-48b25010.js";const r={list:"my-permissions"},t=s=>i.get(`${r.list}/`,s),a=e("user-permissions",{state:()=>({userPermissions:[]}),actions:{async getUserPermisission(){let{data:s}=await t();this.userPermissions=s},canAccess(s){return!0}}});export{a as u};
