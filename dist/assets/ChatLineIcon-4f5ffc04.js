3230d60eb91c80a361a179635af5e3fa659b1363
Azam_John <counterkingbarcelona@gmail.com>
2025-01-21 15:05:26 +0500

    dist deleted

--- dist/assets/ChatLineIcon-4f5ffc04.js
+++
@@ -1,1 +0,0 @@
-const e="/assets/ChatLineIcon-41823e7e.svg";export{e as default};
--- dist/assets/Home-2b325bf9.js
+++
@@ -1,1 +0,0 @@
-import{ak as e,o,c}from"./index-e5f7b630.js";const n={},a={class:"kanban-home flex w-full"};function s(t,r){return o(),c("div",a," Kanban home ")}const f=e(n,[["render",s]]);export{f as default};
