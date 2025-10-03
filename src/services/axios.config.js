f5c33d0b89eee86fffbbf8883e370a2f5336b8e6
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2025-10-03 09:54:07 +0500

    Update axios.config.js

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -11,7 +11,7 @@ import { LANG, ACCESS, REFRESH, EXPIRES, CURRENT_ROUTE } from '@/constants/stora
 import { REDIRECT_AFTER_LOGIN } from '@/constants/storage'
 import { COLOR_TYPES } from '@/enums'
 
-const baseURL = process.env.NODE_ENV === 'development' || window.location.host === 'app.itco.uz' || window.location.host === 'new-side-project.vercel.app' ? import.meta.env.VITE_BASE_TEST_URL : import.meta.env.VITE_BASE_PROD_URL;
+const baseURL =  import.meta.env.VITE_BASE_PROD_URL;
 const axiosParams = { baseURL }
 const axiosInstance = axios.create(axiosParams)
 const axiosConfig = (axiosInstance) => {
